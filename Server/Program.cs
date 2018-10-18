492f7a0f920c634ac85ecbf44ffec4d588392794
uatrade <piskovtsev@gmail.com>
2018-10-18 21:47:15 +0300

    Connect Ok

---
+++ Server/Program.cs
@@ -0,0 +1,112 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Xml;
+
+namespace Server
+{
+    class Program
+    {
+        static int port = 4001;
+
+        static List<string> ReadXML(string findIndex)
+        {
+            List<string> streets = new List<string>();
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(@"D:\Академия Шаг\Сетевое Программирование\ДЗ№1\DZ1\ind.xml");
+                XmlElement xmlRoot = xmlDoc.DocumentElement;
+
+
+                foreach(XmlNode xnode in xmlRoot)
+                {
+                    // получаем атрибут index
+                    if (xnode.Attributes.Count > 0)
+                    {
+                        XmlNode attr = xnode.Attributes.GetNamedItem("index");
+                        if (attr != null)
+
+                            if(findIndex== attr.Value)
+                            {
+                                foreach (XmlNode xchild in xnode.ChildNodes)
+                                {
+                                    if(xchild.Name=="street")
+                                    {
+                                        //Console.WriteLine(xchild.InnerText);
+                                        streets.Add(xchild.InnerText);
+                                    }
+                                }
+                            }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+
+            return streets;
+        }
+        static void Main(string[] args)
+        {
+
+            //IPEndPoint IpEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.105"), port);
+            IPEndPoint IpEndPoint = new IPEndPoint(IPAddress.Any, port);
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                listenSocket.Bind(IpEndPoint);
+                listenSocket.Listen(10);
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    byte[] data = new byte[1024];
+
+                    do
+                    {
+                        bytes = handler.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        if(bytes!=0)
+                            Console.WriteLine("Получение данных");
+
+                    } while (handler.Available>0);
+
+                    List<string> mystreets=ReadXML(builder.ToString());   //Получаем список
+
+                   // List<string> mystreets = ReadXML("50001");   //Получаем список
+
+                    foreach (var item in mystreets)
+                    {
+                        data = Encoding.Unicode.GetBytes(item);
+                        handler.Send(data);
+                    }
+                    // закрываем сокет
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+
+                    //if(builder.ToString()==TODO)
+                    //{
+
+                    //}
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
+
+        }
+    }
+}
