f600599425b9afa303ecddcfe82b6e830e86273d
uatrade <piskovtsev@gmail.com>
2018-10-20 21:00:52 +0300

    Ready

--- Server/Program.cs
+++ Server/Program.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.Xml;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace Server
 {
@@ -20,7 +23,8 @@ namespace Server
             try
             {
                 XmlDocument xmlDoc = new XmlDocument();
-                xmlDoc.Load(@"D:\Академия Шаг\Сетевое Программирование\ДЗ№1\DZ1\ind.xml");
+                //xmlDoc.Load(@"D:\Академия Шаг\Сетевое Программирование\ДЗ№1\DZ1\ind.xml");
+                xmlDoc.Load(@"ind.xml");
                 XmlElement xmlRoot = xmlDoc.DocumentElement;
 
 
@@ -62,11 +66,12 @@ namespace Server
             try
             {
                 listenSocket.Bind(IpEndPoint);
-                listenSocket.Listen(10);
+                listenSocket.Listen(15);
                 Console.WriteLine("Сервер запущен. Ожидание подключений...");
 
                 while (true)
                 {
+
                     Socket handler = listenSocket.Accept();
                     StringBuilder builder = new StringBuilder();
                     int bytes = 0;
@@ -81,30 +86,30 @@ namespace Server
 
                     } while (handler.Available>0);
 
-                    List<string> mystreets=ReadXML(builder.ToString());   //Получаем список
+                    List<string> mystreets = new List<string>();
 
-                   // List<string> mystreets = ReadXML("50001");   //Получаем список
+                      mystreets=ReadXML(builder.ToString());   //Получаем список
+
+                    // List<string> mystreets = ReadXML("50001");   //Получаем список
+
 
                     foreach (var item in mystreets)
                     {
                         data = Encoding.Unicode.GetBytes(item);
                         handler.Send(data);
+                        data = new byte[1024];
                     }
                     // закрываем сокет
+
                     handler.Shutdown(SocketShutdown.Both);
                     handler.Close();
 
-                    //if(builder.ToString()==TODO)
-                    //{
-
-                    //}
                 }
 
             }
             catch (Exception ex)
             {
-
-                
+                Console.WriteLine(ex.Message);  
             }
 
         }
